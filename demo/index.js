0e676a6f47fcb11984f4260997e9204cbda8ca2e
vecxqz <741621270@qq.com>
2020-09-23 23:50:35 +0800

    feat: demo增加在线人员显示
    1. 增加在线人员显示
    2. 增加固定颜色选择

--- demo/index.js
+++ demo/index.js
@@ -5,15 +5,34 @@ const io = require('socket.io')(http)
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/demo.html');
 });
-
+const userList = new Set()
 io.on('connection', (socket) => {
   socket.join('demo', () => {
-    io.to('demo').emit('a new user has joined the demo room')
+    const rooms = Object.keys(socket.rooms);
+    if (!userList.has(rooms[0])) {
+      userList.add(rooms[0])
+    }
+    io.to('demo').emit('message', {
+      type: 'userAdd',
+      data: [...userList]
+    });
   })
+  socket.on('disconnecting', (reason) => {
+    const rooms = Object.keys(socket.rooms);
+    if (userList.has(rooms[0])) {
+      userList.delete(rooms[0])
+    }
+    io.to('demo').emit('message', {
+      type: 'userRemove',
+      data: [...userList]
+    });
+  });
   socket.on('message', (data) => {
     io.emit('message', data);
   })
+
 })
+
 
 http.listen(3000, () => {
   console.log('listening on http://localhost:3000')
