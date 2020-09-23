08b1d46ba3d98f61eedf5987108f5f281cfa2075
vecxqz <741621270@qq.com>
2020-09-23 20:55:03 +0800

    feat: demo增加协同功能

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,20 @@
-const app = require('express')
+const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/demo.html');
 });
+
+io.on('connection', (socket) => {
+  socket.join('demo', () => {
+    io.to('demo').emit('a new user has joined the demo room')
+  })
+  socket.on('message', (data) => {
+    io.emit('message', data);
+  })
+})
+
+http.listen(3000, () => {
+  console.log('listening on http://localhost:3000')
+})
