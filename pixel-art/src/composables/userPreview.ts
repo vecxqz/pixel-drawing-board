949d1910891ea02dc44b3fb9c5da7bd8acc78816
vecxqz <741621270@qq.com>
2020-10-29 18:54:05 +0800

    fix: 画布预览区，背景图和绘制内容不能合并展示的问题

--- pixel-art/src/composables/userPreview.ts
+++ pixel-art/src/composables/userPreview.ts
@@ -6,11 +6,11 @@ export function userPreview() {
     canvasCtxs: Array<CanvasRenderingContext2D>,
     targetcanvasCtx: CanvasRenderingContext2D
   ) {
-    // 这里想将不同的canvas合到一个canvas上
+    // 这里将不同的canvas合到一个canvas上
     const { width, height } = store.state.canvasModule;
     for (let i = 0; i < canvasCtxs.length; i++) {
-      const data = canvasCtxs[i].getImageData(0, 0, width, height);
-      targetcanvasCtx.putImageData(data, 0, 0);
+      const data = canvasCtxs[i].canvas;
+      targetcanvasCtx.drawImage(data, 0, 0);
     }
     store.state.canvasModule.previewUrl = targetcanvasCtx.canvas.toDataURL(
       "image/png",
