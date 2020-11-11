74239d196391ed9feb420092c7395502e49c8920
vecxqz <741621270@qq.com>
2020-11-11 21:05:47 +0800

    refactor: router模式修改为webHistory

--- pixel-art/src/router/index.ts
+++ pixel-art/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
 
 const routes = [
   {
@@ -16,7 +16,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  // history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 });
 
