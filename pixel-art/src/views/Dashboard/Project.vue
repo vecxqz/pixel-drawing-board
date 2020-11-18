5a6411558a4160e44f80d2aba39e1138f1cbaf4e
vecxqz <741621270@qq.com>
2020-11-18 19:43:48 +0800

    feat: 修改工程名，删除工程功能

--- pixel-art/src/views/Dashboard/Project.vue
+++ pixel-art/src/views/Dashboard/Project.vue
@@ -2,7 +2,7 @@
   <div class="project-data">
     <el-card
       class="project-item"
-      v-for="project in projectList"
+      v-for="(project, index) in projectList"
       :key="project.guid"
     >
       <div v-if="project.data">
@@ -10,32 +10,129 @@
           :to="{ name: 'DrawPixelDetail', params: { id: project.guid } }"
         >
           <img :src="project.data.pages[0].previewUrl" alt="" srcset="" />
-          <div class="project-title">{{ project.data.title }}</div>
         </router-link>
+        <div class="project-title">
+          <router-link
+            v-if="!project.edit"
+            :to="{ name: 'DrawPixelDetail', params: { id: project.guid } }"
+          >
+            {{ project.data.title }}
+          </router-link>
+          <el-input
+            v-if="project.edit"
+            ref="elInputRef"
+            @blur="handleEditBlur($event, index)"
+            @keyup.enter="handleEditEnter($event, index)"
+            v-model="project.data.title"
+            :autofocus="true"
+          ></el-input>
+          <div class="op-group">
+            <el-button
+              type="primary"
+              size="mini"
+              icon="el-icon-edit"
+              @click="changeToEditStatus(index)"
+              circle
+            ></el-button>
+            <el-popconfirm
+              title="确定删除该工程？"
+              confirmButtonText="确定"
+              cancelButtonText="取消"
+              @confirm="remove($event, project.guid)"
+            >
+              <template #reference>
+                <el-button
+                  type="danger"
+                  icon="el-icon-delete"
+                  size="mini"
+                  circle
+                ></el-button>
+              </template>
+            </el-popconfirm>
+          </div>
+        </div>
       </div>
     </el-card>
   </div>
 </template>
 
 <script lang="ts">
-import { onMounted, ref } from "vue";
-import { getAllPagesData } from "../../utils/request/canvas";
+import { nextTick, onMounted, ref } from "vue";
+import {
+  getAllPagesData,
+  removePagesData,
+  setPageTitle
+} from "../../utils/request/canvas";
+import { Message } from "element-plus/lib/message";
 export default {
   setup() {
     const projectList = ref([{}]);
+    const elInputRef = ref(null);
+
     onMounted(async () => {
+      await parseProjectList();
+    });
+
+    async function parseProjectList() {
       const {
         data: { data }
       } = await getAllPagesData();
       projectList.value = (data as Array<any>).map((d: any) => {
         return {
           ...d,
-          data: JSON.parse(d.data)
+          data: JSON.parse(d.data),
+          edit: false
         };
       });
-      console.log(projectList.value);
-    });
-    return { projectList };
+    }
+
+    async function remove(event: Event, guid: string) {
+      const { code, msg } = (await removePagesData(guid)) as any;
+      if (code === 200) {
+        Message({
+          type: "success",
+          message: msg
+        });
+        await parseProjectList();
+      }
+    }
+
+    async function handleEditBlur(event: Event, index: number) {
+      const { msg, code } = (await setPageTitle({
+        title: (event.target as any).value,
+        guid: (projectList.value[index] as any).guid
+      })) as any;
+      if (code === 200) {
+        Message({
+          type: "success",
+          message: msg
+        });
+      }
+      (projectList.value[index] as any).edit = false;
+    }
+    function handleEditEnter() {
+      nextTick(() => {
+        const el = elInputRef.value as any;
+        el.blur();
+      });
+    }
+
+    function changeToEditStatus(index: number) {
+      (projectList.value[index] as any).edit = true;
+      nextTick(() => {
+        const el = elInputRef.value as any;
+        el.focus();
+      });
+    }
+
+    return {
+      projectList,
+      remove,
+      handleEditBlur,
+      changeToEditStatus,
+      handleEditEnter,
+      elInputRef
+    };
   }
 };
 </script>
@@ -58,11 +155,23 @@ export default {
     }
   }
   &-title {
+    height: 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     padding: 5px;
-    text-align: center;
-    color: black;
+    a {
+      color: black;
+    }
+    ::v-deep(.el-input) {
+      width: calc(100% - 90px);
+    }
+    .op-group {
+      width: 70px;
+    }
   }
 }
+
 ::v-deep .el-card__body {
   padding: 0;
 }
